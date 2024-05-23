d4bf5109ec497479a065f542b2e1b303679e110c
eranga85 <eranga.ba@gmail.com>
2024-05-23 21:27:26 +0200

    TEST

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 docker-compose up -d
 docker network ls
 docker network inspect [docker-compose-redis-cluster_redis-cluster id]
-docker exec -it redis-cluster-5 redis-cli --cluster create 162.25.0.2:6390 162.25.0.3:6391 162.25.0.4:6392 162.25.0.5:6393 162.25.0.6:6394 162.25.0.7:6395 --cluster-replicas 1 -a Eranga123
+docker exec -it redis-cluster-5 redis-cli --cluster create 172.21.0.2:6380 172.21.0.3:6381 172.21.0.4:6382 172.21.0.5:6383 172.21.0.6:6384 172.21.0.7:6385 --cluster-replicas 1 -a Szzf@2022
