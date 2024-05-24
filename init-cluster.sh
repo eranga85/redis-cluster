ecc9507b735f404ba1bcf72fa360488154f63e3f
eranga85 <eranga.ba@gmail.com>
2024-05-24 16:02:49 +0200

    Update

--- init-cluster.sh
+++
@@ -1,9 +0,0 @@
-# Waits for all Redis nodes to be ready
-echo "Waiting for the Redis nodes to be ready..."
-sleep 10
-
-# Creates the cluster
-echo "Creating the Redis cluster..."
-redis-cli --cluster create 127.0.0.1:6380 127.0.0.1:6381 127.0.0.1:6382 \
- 127.0.0.1:6383 127.0.0.1:6384 127.0.0.1:6385 \
- --cluster-replicas 1 --cluster-yes -a Eranga123
--- start_cluster.sh
+++ start_cluster.sh
@@ -2,5 +2,4 @@
 docker compose down
 docker compose up -d
 docker network ls
-#docker network inspect [docker-compose-redis-cluster_redis-cluster id]
 docker exec -it node1 redis-cli -c -p 6380 -a Eranga123
