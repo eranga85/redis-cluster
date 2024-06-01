60f7250bc3b572917baaba010d3c5edcb22de4d4
eranga85 <eranga.ba@gmail.com>
2024-06-01 14:47:52 +0200

    update

--- HSET.sh
+++ HSET.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 REDIS_HOST="127.0.0.1"
-REDIS_PORT="6380"
-REDIS_PASSWORD="Eranga123"
+REDIS_PORT="6379"
+REDIS_PASSWORD="Redis6379"
 SYLLABLES=("ba" "bo" "be" "da" "do" "de" "la" "lo" "le" "ma" "mo" "me" "na" "no" "ne" "pa" "po" "pe" "ra" "ro" "re" "sa" "so" "se" "ta" "to" "te")
 
 # Function to generate a random name
@@ -21,7 +21,7 @@ for ((i=1; i<=100; i++)); do
     random_name=$(generate_random_name $((RANDOM % 3 + 2)))  # Random length between 2 and 4 syllables
 
     redis-cli -h $REDIS_HOST -p $REDIS_PORT -a $REDIS_PASSWORD HSET card_info "$field" "$random_name"
-    redis-cli ZADD my_sorted_set $(date +%s) $field
+    redis-cli -h $REDIS_HOST -p $REDIS_PORT -a $REDIS_PASSWORD ZADD my_sorted_set $(date +%s) $field
 done
 
 echo "Insertion of 100 random names into Redis hash completed."
